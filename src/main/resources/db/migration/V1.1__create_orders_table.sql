bc836a32074dea0cb6f68760ae9b265645b9ead6
Orzech <hoch.mat@gmail.com>
2023-12-29 17:40:07 +0100

    Created Users Table in DB with flyway. Change Byte[] to byte[] in product due to some hibernate issues.

--- src/main/resources/db/migration/V1.1__create_orders_table.sql
+++ src/main/resources/db/migration/V1.1__create_orders_table.sql
@@ -1,11 +1,17 @@
-CREATE TABLE ORDERS
-(
-	ID bigint PRIMARY KEY unique not null auto_increment,
-	ORDER_DATE datetime,
-    ADMISSION_DATE datetime,
-    PAYMENT_DATE datetime,
-    REALIZATION_DATE datetime,
-    TOTAL_PRICE double,
-    USER_ID bigint,
-    FOREIGN KEY (USER_ID) REFERENCES USERS(ID)
-)
+--CREATE TABLE USERS
+--(
+--	ID bigint PRIMARY KEY unique not null auto_increment,
+--	LOGIN varchar(20),
+--    PASSWORD varchar(20)
+--)
+--CREATE TABLE ORDERS
+--(
+--	ID bigint PRIMARY KEY unique not null auto_increment,
+--	ORDER_DATE datetime,
+--    ADMISSION_DATE datetime,
+--    PAYMENT_DATE datetime,
+--    REALIZATION_DATE datetime,
+--    TOTAL_PRICE double,
+--    USER_ID bigint,
+--    FOREIGN KEY (USER_ID) REFERENCES USERS(ID)
+--)
