6644840ce12b6924bb4595a16031f7d56798c28c
Orzech <hoch.mat@gmail.com>
2023-12-29 19:27:16 +0100

    Corrected create orders table file

--- src/main/resources/db/migration/V1.1__create_orders_table.sql
+++ src/main/resources/db/migration/V1.1__create_orders_table.sql
@@ -1,17 +1,11 @@
---CREATE TABLE USERS
---(
---	ID bigint PRIMARY KEY unique not null auto_increment,
---	LOGIN varchar(20),
---    PASSWORD varchar(20)
---)
---CREATE TABLE ORDERS
---(
---	ID bigint PRIMARY KEY unique not null auto_increment,
---	ORDER_DATE datetime,
---    ADMISSION_DATE datetime,
---    PAYMENT_DATE datetime,
---    REALIZATION_DATE datetime,
---    TOTAL_PRICE double,
---    USER_ID bigint,
---    FOREIGN KEY (USER_ID) REFERENCES USERS(ID)
---)
+CREATE TABLE ORDERS
+(
+	ID bigint PRIMARY KEY unique not null auto_increment,
+	ORDER_DATE datetime,
+    ADMISSION_DATE datetime,
+    PAYMENT_DATE datetime,
+    REALIZATION_DATE datetime,
+    TOTAL_PRICE double,
+    USER_ID bigint,
+    FOREIGN KEY (USER_ID) REFERENCES USERS(ID)
+)
