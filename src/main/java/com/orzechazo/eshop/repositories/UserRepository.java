eed8255def2c00265bf5ca3b7cebe4869203c888
Orzech <hoch.mat@gmail.com>
2023-12-30 13:19:32 +0100

    Corrected UserRepository

--- src/main/java/com/orzechazo/eshop/repositories/UserRepository.java
+++ src/main/java/com/orzechazo/eshop/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.orzechazo.eshop.repositories;
 
+import com.orzechazo.eshop.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserRepository, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
