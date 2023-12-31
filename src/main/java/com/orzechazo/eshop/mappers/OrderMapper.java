92d146b0854025e82362dace8373be814cfdc427
Orzechazo <148963111+Orzechazo@users.noreply.github.com>
2023-12-31 12:33:31 +0100

    Created OrderMapper (#49)

---
+++ src/main/java/com/orzechazo/eshop/mappers/OrderMapper.java
@@ -0,0 +1,16 @@
+package com.orzechazo.eshop.mappers;
+
+import com.orzechazo.eshop.domain.Order;
+import com.orzechazo.eshop.domain.dto.OrderDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface OrderMapper {
+
+    OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
+
+    Order orderDtoToOrder(OrderDto orderDto);
+
+    OrderDto orderToOrderDto(Order order);
+}
