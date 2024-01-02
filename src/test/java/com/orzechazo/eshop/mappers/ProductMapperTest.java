f16f26d58c15e8d8af0b5ec472dd60b168395e65
Orzechazo <148963111+Orzechazo@users.noreply.github.com>
2024-01-02 11:29:51 +0100

    Created tests for ProductMapper (#58)

---
+++ src/test/java/com/orzechazo/eshop/mappers/ProductMapperTest.java
@@ -0,0 +1,73 @@
+package com.orzechazo.eshop.mappers;
+
+import com.orzechazo.eshop.domain.Product;
+import com.orzechazo.eshop.domain.dto.ProductDto;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductMapperTest {
+
+    ProductMapper mapper = ProductMapper.INSTANCE;
+
+    @Test
+    void productToProductDto() {
+        //given
+        Product product = new Product();
+        product.setName("test");
+        product.setAmount(5);
+        product.setDescription("testDescription");
+        product.setGrossPrice(new BigDecimal("2"));
+        product.setNetPrice(new BigDecimal("1.5"));
+        product.setId(1L);
+        product.setImage(new byte[0]);
+        //when
+        ProductDto mappedDto = mapper.productToProductDto(product);
+        //then
+        assertNotNull(mappedDto);
+        assertEquals("test",mappedDto.getName());
+        assertEquals("testDescription",mappedDto.getDescription());
+        assertEquals(5,mappedDto.getAmount());
+        assertEquals(1L,mappedDto.getId());
+        assertEquals(new BigDecimal("1.5"),mappedDto.getNetPrice());
+        assertEquals(new BigDecimal("2"),mappedDto.getGrossPrice());
+        assertNotNull(mappedDto.getImage());
+    }
+
+    @Test
+    void productDtoToProduct() {
+        //given
+        ProductDto productDto = ProductDto.builder().name("test").amount(5).description("testDescription")
+                .id(1L).netPrice(new BigDecimal("1.5")).grossPrice(new BigDecimal("2")).image(new byte[0])
+                .build();
+        //when
+        Product mappedProduct = mapper.productDtoToProduct(productDto);
+        //then
+        assertNotNull(mappedProduct);
+        assertEquals("test",mappedProduct.getName());
+        assertEquals("testDescription",mappedProduct.getDescription());
+        assertEquals(5,mappedProduct.getAmount());
+        assertEquals(1L,mappedProduct.getId());
+        assertEquals(new BigDecimal("1.5"),mappedProduct.getNetPrice());
+        assertEquals(new BigDecimal("2"),mappedProduct.getGrossPrice());
+        assertNotNull(mappedProduct.getImage());
+
+    }
+
+    @Test
+    void productToProductDtoNull() {
+        //when
+        ProductDto mappedDto = mapper.productToProductDto(null);
+        //then
+        assertNull(mappedDto);
+    }
+    @Test
+    void productDtoToProductNull() {
+        //when
+        Product mappedProduct = mapper.productDtoToProduct(null);
+        //then
+        assertNull(mappedProduct);
+    }
+}
