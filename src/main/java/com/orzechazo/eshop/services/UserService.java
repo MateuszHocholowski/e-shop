3020b12a292bbfcaf59eb54ab4da80c852d93656
Orzech <hoch.mat@gmail.com>
2024-01-02 16:01:33 +0100

    Created methods in UserService interface

--- src/main/java/com/orzechazo/eshop/services/UserService.java
+++ src/main/java/com/orzechazo/eshop/services/UserService.java
@@ -1,4 +1,16 @@
 package com.orzechazo.eshop.services;
 
+import com.orzechazo.eshop.domain.User;
+import com.orzechazo.eshop.domain.dto.UserDto;
+
+import java.util.List;
+
 public interface UserService {
+    List<UserDto> getAllUsers();
+    UserDto getUserById(Long id);
+    UserDto createUser(UserDto userDto);
+    UserDto updateUser(Long id, UserDto userDto);
+    UserDto saveUserAndReturnDto(User user);
+    void deleteUserById(Long id);
+
 }
--- src/main/java/com/orzechazo/eshop/services/UserServiceImpl.java
+++ src/main/java/com/orzechazo/eshop/services/UserServiceImpl.java
@@ -1,8 +1,12 @@
 package com.orzechazo.eshop.services;
 
+import com.orzechazo.eshop.domain.User;
+import com.orzechazo.eshop.domain.dto.UserDto;
 import com.orzechazo.eshop.mappers.UserMapper;
 import com.orzechazo.eshop.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -12,5 +16,36 @@ public class UserServiceImpl implements UserService{
 
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+
+        return null;
+    }
+
+    @Override
+    public UserDto getUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        return null;
+    }
+
+    @Override
+    public UserDto updateUser(Long id, UserDto userDto) {
+        return null;
+    }
+
+    @Override
+    public UserDto saveUserAndReturnDto(User user) {
+        return null;
+    }
+
+    @Override
+    public void deleteUserById(Long id) {
+
     }
 }
