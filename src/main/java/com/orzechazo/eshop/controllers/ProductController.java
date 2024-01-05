30dcadfd7b88237cfd335b0dc147d0e88e86e1ec
Orzechazo <148963111+Orzechazo@users.noreply.github.com>
2024-01-05 09:35:20 +0100

    Created ProductController with methods and tests. (#87)
    * Created ProductController with methods and tests.
    
    * Requested changes

---
+++ src/main/java/com/orzechazo/eshop/controllers/ProductController.java
@@ -0,0 +1,43 @@
+package com.orzechazo.eshop.controllers;
+
+import com.orzechazo.eshop.domain.dto.ProductDto;
+import com.orzechazo.eshop.services.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductDto> getAllProducts() {
+        return productService.getAllProducts();
+    }
+    @GetMapping("/{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProductDto getProductByName(@PathVariable String name) {
+        return productService.getProductDtoByName(name);
+    }
+    @PutMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductDto createProduct(@RequestBody ProductDto productDto) {
+        return productService.createProduct(productDto);
+    }
+    @PostMapping("/update/{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProductDto updateProduct(@PathVariable String name,
+                                    @RequestBody ProductDto productDto) {
+        return productService.updateProduct(name, productDto);
+    }
+    @DeleteMapping("/delete/{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteProduct(@PathVariable String name) {
+        productService.deleteProductByProductName(name);
+    }
+}
