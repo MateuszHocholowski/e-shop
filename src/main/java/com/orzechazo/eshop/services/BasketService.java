f19f16de5c364e09d0fc8a599d5ce9aa7ff06786
Orzech <hoch.mat@gmail.com>
2024-01-29 10:42:21 +0100

    Made changes to BasketService - Made condition to check product amount - Removed overloaded methods from Service - created getAllBasketProducts method

--- src/main/java/com/orzechazo/eshop/services/BasketService.java
+++ src/main/java/com/orzechazo/eshop/services/BasketService.java
@@ -1,6 +1,9 @@
 package com.orzechazo.eshop.services;
 
 import com.orzechazo.eshop.domain.dto.BasketDto;
+import com.orzechazo.eshop.domain.dto.ProductDto;
+
+import java.util.Map;
 
 public interface BasketService {
     BasketDto getBasketDtoByBasketId(String basketId);
@@ -8,9 +11,8 @@ public interface BasketService {
     BasketDto updateBasket(BasketDto basketDto);
     void deleteBasket(String basketId);
     BasketDto addProductToBasket(String productName, String basketId, int amount);
-    BasketDto addProductToBasket(String productName, String basketId);
     BasketDto subtractProductFromBasket(String productName, String basketId, int amount);
-    BasketDto subtractProductFromBasket(String productName, String basketId);
     BasketDto removeProductFromBasket(String productName, String basketId);
+    Map<ProductDto, Integer> fetchAllProductsFromBasket(String basketId);
 }
 
--- src/test/java/com/orzechazo/eshop/services/BasketServiceImplTest.java
+++ src/test/java/com/orzechazo/eshop/services/BasketServiceImplTest.java
@@ -3,6 +3,7 @@ package com.orzechazo.eshop.services;
 import com.orzechazo.eshop.domain.Basket;
 import com.orzechazo.eshop.domain.Product;
 import com.orzechazo.eshop.domain.dto.BasketDto;
+import com.orzechazo.eshop.domain.dto.ProductDto;
 import com.orzechazo.eshop.exceptions.BadRequestException;
 import com.orzechazo.eshop.exceptions.ResourceNotFoundException;
 import com.orzechazo.eshop.repositories.BasketRepository;
@@ -14,9 +15,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -26,6 +29,7 @@ class BasketServiceImplTest {
 
     public static final String BASKET_ID = "1";
     private static final String PRODUCT_NAME = "productName";
+    private static final String PRODUCT2_NAME = "product2";
     @InjectMocks
     private BasketServiceImpl basketService;
     @Mock
@@ -140,7 +144,7 @@ class BasketServiceImplTest {
         when(basketRepository.findByBasketId(any())).thenReturn(Optional.of(basket));
         when(basketRepository.save(any())).thenReturn(updatedBasket);
         //when
-        BasketDto updatedDto = basketService.addProductToBasket(PRODUCT_NAME, BASKET_ID);
+        BasketDto updatedDto = basketService.addProductToBasket(PRODUCT_NAME, BASKET_ID, 1);
         //then
         assertEquals(1, updatedDto.getProductNamesMap().get(PRODUCT_NAME));
         verify(basketRepository,times(1)).save(any());
@@ -164,7 +168,7 @@ class BasketServiceImplTest {
         when(basketRepository.findByBasketId(any())).thenReturn(Optional.of(basket));
         when(basketRepository.save(any())).thenReturn(updatedBasket);
         //when
-        BasketDto updatedDto = basketService.addProductToBasket(PRODUCT_NAME, BASKET_ID);
+        BasketDto updatedDto = basketService.addProductToBasket(PRODUCT_NAME, BASKET_ID, 1);
         //then
         assertEquals(productAmountInBasket + 1, updatedDto.getProductNamesMap().get(PRODUCT_NAME));
         verify(basketRepository,times(1)).save(any());
@@ -212,7 +216,7 @@ class BasketServiceImplTest {
         when(basketRepository.findByBasketId(any())).thenReturn(Optional.of(basket));
         when(basketRepository.save(any())).thenReturn(updatedBasket);
         //when
-        BasketDto updatedDto = basketService.subtractProductFromBasket(PRODUCT_NAME,BASKET_ID);
+        BasketDto updatedDto = basketService.subtractProductFromBasket(PRODUCT_NAME,BASKET_ID, 1);
         //then
         assertEquals(productAmountInBasket-1,updatedDto.getProductNamesMap().get(PRODUCT_NAME));
         verify(basketRepository, times(1)).save(any());
@@ -240,5 +244,27 @@ class BasketServiceImplTest {
         //then
         assertFalse(updatedDto.getProductNamesMap().containsKey(PRODUCT_NAME));
         verify(basketRepository,times(1)).save(any());
+    }
+
+    @Test
+    void fetchAllProductsFromBasket() {
+        //given
+        Product product1 = new Product();
+        product1.setName(PRODUCT_NAME);
+
+        Product product2 = new Product();
+        product2.setName(PRODUCT2_NAME);
+
+        Basket basketInDb = new Basket();
+        basketInDb.setBasketId(BASKET_ID);
+        basketInDb.setProducts(Map.of(product1, 1, product2, 2));
+        when(basketRepository.findByBasketId(any())).thenReturn(Optional.of(basketInDb));
+        //when
+        Map<ProductDto, Integer> returnedProductMap = basketService.fetchAllProductsFromBasket(BASKET_ID);
+        List<String> returnedProductNameList = returnedProductMap.keySet().stream()
+                .map(ProductDto::getName).toList();
+        //then
+        assertThat(returnedProductNameList)
+                .containsExactlyInAnyOrderElementsOf(List.of(PRODUCT_NAME, PRODUCT2_NAME));
     }
 }
